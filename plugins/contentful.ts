03d0da15be7738446e5aa0c5389a235d8a26af19
Walter Castro <walterkstro@gmail.com>
2023-09-01 19:59:52 -0600

    Aplicado el operador ternario para crear cliente de contentful

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,14 +1,19 @@
 import { ContentfulClientApi, CreateClientParams, createClient } from "contentful"
+import contentful from "contentful"
+
 export default defineNuxtPlugin((nuxtApp) => {
     const runtimeConfig = useRuntimeConfig();
 
+    const creatingClient = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+    
     const config:CreateClientParams = {
         space: runtimeConfig.public.spaceId,
-        accessToken: runtimeConfig.public.token
+        accessToken: runtimeConfig.public.token,
+        environment:'master'
     }
 
-    const contentful:ContentfulClientApi<undefined> = createClient(config)
-
+    const contentful:ContentfulClientApi<any> = creatingClient(config)
+    
     return {
         provide: {
             contentful
