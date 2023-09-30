957a7d60a410836419c1e821a1fe4558f4eff1e5
Walter Castro <walterkstro@gmail.com>
2023-09-30 17:21:01 -0600

    Agregado la funcionalidad de formulario de contacto

--- components/home/contact/Form.vue
+++ components/home/contact/Form.vue
@@ -1,12 +1,40 @@
 <script lang="ts" setup>
-const Container = defineAsyncComponent(() => import('@c/layout/Container.vue'));
-const Button = defineAsyncComponent(() => import('@c/global/Button.vue'));
-const submited = ref(false)
-const status = ref(false)
+import axios from 'axios';
+import { reset } from '@formkit/core'
 
-const event = (value: any) => {
-    console.log(value)
+const Container     = defineAsyncComponent(() => import('@c/layout/Container.vue'));
+const Message       = defineAsyncComponent(() => import('@c/home/contact/Message.vue'));
+const Spiner        = defineAsyncComponent(() => import('@c/home/contact/Spiner.vue'));
+const submited      = ref(false)
+const showMessage   = ref(false)
+let statusSubmited  = reactive({
+    status:true,
+    message:'Gracias, por tu mensaje!. Espero responderte lo más pronto posible'
+})
+
+async function handlerSubmit( input:any ) {
+    submited.value  = true
+    try {
+        await axios.post('https://formeezy.com/api/v1/forms/63aa1a57b59e48000842fb54/submissions',{...input})
+
+    } catch (error) {
+        statusSubmited.status = false
+        statusSubmited.message = 'Lo lamento, pero tu mensaje no se ha podido enviar, intenta mas tarde'
+    }finally{
+        showMessage.value = true
+        submited.value  = false
+        reset('contacto')
+        removeMessage()
+    }
 }
+
+function removeMessage(){
+    setTimeout(()=>{
+        showMessage.value = false
+    },5000)
+}
+
+const event = (value: any) => {console.log(value)}
 </script>
 <template>
     <section class="py-8 lg:py-16 bg-light">
@@ -14,9 +42,13 @@ const event = (value: any) => {
         <Container>
             <h2 class="h2 text-center text-dark">Contactame</h2>
             <FormKit type="form" :actions="false" #default="{ state: { valid } }" id="contacto"
-                form-class="max-w-xs mx-auto md:max-w-md lg:max-w-lg" messages="text-sm text-gray-900" :on-input="event">
+                form-class="max-w-xs mx-auto md:max-w-md lg:max-w-lg" messages="text-sm text-gray-900" :on-input="event" @submit="handlerSubmit">
 
-                <FormKit type="text" name="name" label="Nombre" validation="required"
+                <FormKit 
+                    type="text" 
+                    name="name" 
+                    label="Nombre" 
+                    validation="required"
                     :validation-messages="{ required: 'Este campo es requerido' }" 
                     id="name" outer-class="mb-6"
                     placeholder="Juan Perez" label-class="block mb-2 text-base font-medium text-gray-900 dark:text-white"
@@ -24,7 +56,11 @@ const event = (value: any) => {
                     :on-input="event"
                     message-class="validation-message"/>
 
-                <FormKit type="email" name="email" label="Correo electrónico" validation="required|email"
+                <FormKit 
+                    type="email" 
+                    name="email" 
+                    label="Correo electrónico" 
+                    validation="required|email"
                     :validation-messages="{ required: 'Este campo es requerido',email:'Ingrese un correo electrónico válido' }" 
                     id="name" placeholder="jperez@ejemplo.com"
                     outer-class="mb-6" label-class="block mb-2 text-base font-medium text-gray-900 dark:text-white"
@@ -32,7 +68,11 @@ const event = (value: any) => {
                     :on-input="event"
                     message-class="validation-message"/>
 
-                <FormKit type="textarea" name="email" rows="5" label="Deja tu mensaje" validation="required"
+                <FormKit 
+                    type="textarea" 
+                    name="message" 
+                    rows="5" label="Deja tu mensaje" 
+                    validation="required"
                     :validation-messages="{ required: 'Este campo es requerido' }" 
                     id="message"
                     placeholder="Hola mucho gusto..." outer-class="mb-6"
@@ -41,11 +81,17 @@ const event = (value: any) => {
                     :on-input="event"
                     message-class="validation-message"/>
 
-                <FormKit type="submit" :on-input="event">
-                    <Button path="profile" class="text-light bg-dark">Enviar mensaje</Button>
+                <FormKit 
+                    type="submit" 
+                    :on-input="event"
+                    input-class="text-light bg-dark btn"
+                    v-if="valid"
+                >
+                    <Spiner v-if="submited"/> Enviar mensaje
                 </FormKit>
 
             </FormKit>
+            <Message v-if="showMessage" :="statusSubmited"/>
         </Container>
     </section>
 </template>
--- components/home/contact/Message.vue
+++ components/home/contact/Message.vue
@@ -1,15 +1,17 @@
+<script lang="ts" setup>
+interface Props {
+    message:string,
+    status:boolean
+}
+const props = defineProps<Props>()
+
+const typeAlert = computed(() => {
+    return props.status ? 'text-green-700 bg-green-100' : 'text-red-700 bg-red-100'
+})
+</script>
 <template>
-  <div>
-    
-  </div>
+    <div class="p-4 mb-4 text-sm rounded-lg min-w-min max-w-max mx-auto" role="alert" :class="typeAlert">
+        🙏 <span class="font-medium">{{ message }}</span>
+    </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
-
-
-<style>
-
-</style>
